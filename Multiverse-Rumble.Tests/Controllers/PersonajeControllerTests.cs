a818b5baf8b8c69a83687826f5f7058f2691e8e5
Gael magaña chan <gael.magana@tecdesoftware.edu.mx>
2026-07-22 20:11:35 -0600

    Update expected character name in test

--- Multiverse-Rumble.Tests/Controllers/PersonajeControllerTests.cs
+++ Multiverse-Rumble.Tests/Controllers/PersonajeControllerTests.cs
@@ -19,7 +19,7 @@ namespace Multiverse_Rumble.Tests.Controllers
             // Assert
             var viewResult = Assert.IsType<ViewResult>(resultado);
             var personaje = Assert.IsType<Personaje>(viewResult.Model);
-            Assert.Equal("Goku", personaje.Nombre);
+            Assert.Equal("Alguien que no existe", personaje.Nombre);
         }
 
         [Fact]
